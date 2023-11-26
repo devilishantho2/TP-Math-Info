0fd147620c69939d556171a741d0fed92ff00e0c
devilishantho2 <anthony.ursin@yahoo.com>
2023-11-26 11:33:22 +0100

    Rubik debug
    Ajout de la fonction debug_rubik(rubik)

--- Rubik's/fonctions_debug.py
+++ Rubik's/fonctions_debug.py
@@ -23,8 +23,62 @@ from fonctions_logique import *
                   # ~ D
 def debug_rubik(rubik):
 			
-	# TODO
-	return
+    haut = []
+    for y in range(TAILLE-1,-1,-1):
+        ligne = ""
+        for x in range(TAILLE):
+            ligne = ligne+rubik[x][y][TAILLE-1]+"|"
+        haut.append(ligne)
+
+    gauche = []
+    for z in range(TAILLE-1,-1,-1):
+        ligne = ""
+        for y in range(TAILLE-1,-1,-1):
+            ligne = ligne+rubik[0][y][z]+"|"
+        gauche.append(ligne)
+        
+    devant = []
+    for z in range(TAILLE-1,-1,-1):
+        ligne = ""
+        for x in range(TAILLE):
+            ligne = ligne+rubik[x][0][z]+"|"
+        devant.append(ligne)
+        
+    droite = []
+    for z in range(TAILLE-1,-1,-1):
+        ligne = ""
+        for y in range(TAILLE):
+            ligne = ligne+rubik[TAILLE-1][y][z]+"|"
+        droite.append(ligne)
+        
+    derriere = []
+    for z in range(TAILLE-1,-1,-1):
+        ligne = ""
+        for x in range(TAILLE-1,-1,-1):
+            ligne = ligne+rubik[x][TAILLE-1][z]+"|"
+        derriere.append(ligne)
+        
+    bas = []
+    for y in range(TAILLE):
+        ligne = ""
+        for x in range(TAILLE):
+            ligne = ligne+rubik[x][y][0]+"|"
+        bas.append(ligne)
+        
+    rubik_text = ''
+    for k in range(TAILLE-1):
+        rubik_text += '             '+str(haut[k])+'\n'
+    rubik_text += '             '+str(haut[TAILLE-1])+'\n                  U\n'
+    
+    for k in range(TAILLE):
+        rubik_text += str(gauche[k])+' '+str(devant[k])+' '+str(droite[k])+' '+str(derriere[k])+'\n'
+    rubik_text += '     L            F            R            B\n'
+    
+    for k in range(TAILLE-1):
+        rubik_text += '             '+str(bas[k])+'\n'
+    rubik_text += '             '+str(bas[TAILLE-1])+'\n                  D\n'
+    
+    print(rubik_text)
 
 # affiche une face en mode debug
 # par exemple :
